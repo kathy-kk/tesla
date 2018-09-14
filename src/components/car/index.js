6fd51713e1a8f75b404652365607c59c17a78397
kathy y <meimeiy1121@gmail.com>
2018-09-14 11:23:15 +1000

    spinning wheels

--- src/components/car/index.js
+++ src/components/car/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import styles from './car.css';
 
 const Car = () => (
-    <div className = {styles.car}></div>
+    <div className = {styles.car}>
+        <div className = {styles.wheels}>
+            <div className = {[styles.wheel, styles.front, styles.run].join(' ')}></div>
+            <div className = {[styles.wheel, styles.rear, styles.run].join(' ')}></div>
+        </div>
+    </div>
 );
 export default Car;
